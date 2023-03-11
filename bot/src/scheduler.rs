b09d90d153a59451a9674771c0fd4228233ac4d0
Perondas <Pperondas@gmail.com>
2023-03-11 10:10:39 +0100

    Updated scheduler

--- bot/src/scheduler.rs
+++ bot/src/scheduler.rs
@@ -5,7 +5,7 @@ use itertools::Itertools;
 use mysql::Pool;
 use poise::serenity_prelude::{CacheAndHttp, GuildId};
 use tokio::{sync::RwLock, task::JoinHandle, time::sleep};
-use tracing::{info, warn};
+use tracing::{debug, error, info, warn};
 
 use crate::{db, steam, Error};
 
@@ -26,7 +26,7 @@ impl Scheduler {
     }
 
     pub fn remove(&self, guild_id: u64) {
-        info!("Removing cron job for guild: {}", guild_id);
+        info!("Removing tracking job for guild: {}", guild_id);
         if let Some((_, job)) = self.jobs.remove(&guild_id) {
             job.abort();
         }
@@ -39,29 +39,21 @@ impl Scheduler {
         let count = schedules.len();
 
         info!(
-            "Starting {} cron jobs",
+            "Starting {} tracking jobs",
             schedules.iter().filter_map(|j| j.1).count()
         );
 
         for (guild_id, schedule) in schedules {
             if let Some(hours) = schedule {
                 let s = self.clone();
-                let h = tokio::spawn(async move {
-                    work_loop(s, guild_id, hours)
-                        .await
-                        // TODO: Handle a failed cron job
-                        // Maybe restart it?
-                        .expect("Cron job failed");
-                });
-                if let Some(old) = self.jobs.insert(guild_id, h) {
-                    old.abort();
-                }
+                s.start_job(guild_id, hours);
+
                 // Spread out the registrations so we don't hit any rate limits
-                sleep(Duration::from_secs(((60 * 60) / count) as u64)).await;
+                sleep(Duration::from_secs(((60 * 30) / count) as u64)).await;
             }
         }
 
-        info!("Started all cron jobs");
+        info!("Started all tracking jobs");
         Ok(())
     }
 
@@ -70,29 +62,52 @@ impl Scheduler {
             .ok_or("No schedule set for this server.")?;
 
         let s = self.clone();
-        let h = tokio::spawn(async move {
-            work_loop(s, guild_id, hours)
-                .await
-                .expect("Cron job failed");
-        });
-        if let Some(old) = self.jobs.insert(guild_id, h) {
-            // If there was already a job running, abort it
-            old.abort();
-        }
+        s.start_job(guild_id, hours);
 
         Ok(())
     }
 
+    fn start_job(self, guild_id: u64, hours: u64) {
+        let s = self.clone();
+        let h = tokio::spawn(async move {
+            match work_loop(s.clone(), guild_id, hours).await {
+                Ok(_) => {}
+                Err(e) => {
+                    error!(
+                        "Tracking job for guild: {} failed with error: {}",
+                        guild_id, e
+                    );
+                }
+            }
+
+            // No matter what happens, we remove the job from the list
+            s.remove(guild_id);
+        });
+
+        if let Some(old) = self.jobs.insert(guild_id, h) {
+            // Abort the old job if it exists
+            old.abort();
+        }
+    }
+
     pub fn is_running(&self, guild_id: u64) -> bool {
+        debug!(guild_id, "Checking if tracking job is running");
         self.jobs.contains_key(&guild_id)
     }
 }
 
 async fn work_loop(s: Scheduler, guild_id: u64, hours: u64) -> Result<(), Error> {
+    info!("Starting tracking job for guild: {}", guild_id);
+
+    let mut interval = tokio::time::interval(Duration::from_secs(60 * 60 * hours));
+
+    // We use delay as we don't care about precision, we just want to tick every so often
+    interval.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Delay);
+
     loop {
         if !db::servers::check_still_in_guild(&s.pool, guild_id)? {
             warn!(
-                "Guild {} is no longer in the guild list, stopping cron job",
+                "Guild {} is no longer in the guild list, stopping tracking job",
                 guild_id
             );
             break;
@@ -109,103 +124,103 @@ async fn work_loop(s: Scheduler, guild_id: u64, hours: u64) -> Result<(), Error>
             }
         }
         db::servers::update_last_update_timestamp(&s.pool, guild_id)?;
-        sleep(Duration::from_secs(hours * 60 * 60)).await;
-    }
 
-    // We need to remove ourself from the jobs list
-    // We do this in a separate task so we don't kill ourselves
-    tokio::spawn(async move {
-        s.remove(guild_id);
-    });
+        interval.tick().await;
+    }
 
     Ok(())
 }
 
 async fn notify_on_updates(scheduler: Scheduler, guild_id: u64) -> Result<(), Error> {
     let client = scheduler.client.read().await;
-    if let Some(client) = &*client {
-        let subscriptions =
-            db::subscriptions::get_all_subscriptions_of_guild(&scheduler.pool, guild_id)?;
 
-        let mut updated = Vec::new();
-        let mut unknown = Vec::new();
-        let mut failed = Vec::new();
-
-        for (last_notify, mod_info) in subscriptions {
-            let mod_info = steam::get_mod(&scheduler.pool, mod_info.id).await?;
-
-            if mod_info.last_updated > last_notify {
-                updated.push((mod_info, last_notify));
-            } else {
-                unknown.push((last_notify, mod_info));
-            }
+    let client = match &*client {
+        Some(c) => c,
+        None => {
+            warn!("Client not set, skipping update check");
+            return Ok(());
         }
+    };
 
-        for (last_notify, mod_info) in unknown {
-            if let Ok(info) = steam::get_latest_mod(&scheduler.pool, mod_info.id).await {
-                if info.last_updated > last_notify {
-                    updated.push((info, last_notify));
-                }
-            } else {
-                failed.push((last_notify, mod_info));
-            }
-        }
+    let subscriptions =
+        db::subscriptions::get_all_subscriptions_of_guild(&scheduler.pool, guild_id)?;
 
-        let update_channel = db::servers::get_update_channel(&scheduler.pool, guild_id)?
-            .ok_or("No update channel set")?;
+    let mut updated = Vec::new();
+    let mut unknown = Vec::new();
+    let mut failed = Vec::new();
 
-        let id = GuildId(guild_id);
-        let g = id.to_partial_guild(&client.http).await?;
+    for (last_notify, mod_info) in subscriptions {
+        let mod_info = steam::get_mod(&scheduler.pool, mod_info.id).await?;
 
-        let channels = g.channels(&client.http).await?;
-
-        let c = match channels.iter().find(|c| c.0 .0 == update_channel) {
-            Some(c) => c.1,
-            None => {
-                return Err(format!(
-                    "Update channel {} not found in guild {}",
-                    update_channel, guild_id
-                )
-                .into())
-            }
-        };
-
-        if updated.is_empty() {
-            info!("No updates for guild: {}", guild_id);
+        if mod_info.last_updated > last_notify {
+            updated.push((mod_info, last_notify));
         } else {
-            info!("Found {} updates for guild: {}", updated.len(), guild_id);
-            if updated.len() > 5 {
-                send_in_chunks_updates(c, client, &updated).await?;
-            } else {
-                send_in_one_updates(c, client, &updated).await?;
-            }
-
-            for (mod_info, _) in updated {
-                db::subscriptions::update_last_notify(&scheduler.pool, guild_id, mod_info.id)?;
-            }
+            unknown.push((last_notify, mod_info));
         }
+    }
 
-        if !failed.is_empty() {
-            c.send_message(&client, |d| {
-                d.content("The following Items could not be updated:".to_string());
-
-                for (_, mod_info) in failed.iter() {
-                    d.add_embed(|e| {
-                        e.title(format!("{}, Id: {}", mod_info.name.clone(), mod_info.id));
-                        e.url(format!(
-                            "https://steamcommunity.com/sharedfiles/filedetails/?id={}",
-                            mod_info.id
-                        ));
-                        e
-                    });
-                }
-
-                d
-            })
-            .await?;
+    for (last_notify, mod_info) in unknown {
+        if let Ok(info) = steam::get_latest_mod(&scheduler.pool, mod_info.id).await {
+            if info.last_updated > last_notify {
+                updated.push((info, last_notify));
+            }
+        } else {
+            failed.push((last_notify, mod_info));
         }
+    }
+
+    let update_channel = db::servers::get_update_channel(&scheduler.pool, guild_id)?
+        .ok_or("No update channel set")?;
+
+    let id = GuildId(guild_id);
+    let g = id.to_partial_guild(&client.http).await?;
+
+    let channels = g.channels(&client.http).await?;
+
+    let c = match channels.iter().find(|c| c.0 .0 == update_channel) {
+        Some(c) => c.1,
+        None => {
+            return Err(format!(
+                "Update channel {} not found in guild {}",
+                update_channel, guild_id
+            )
+            .into())
+        }
+    };
+
+    if updated.is_empty() {
+        info!("No updates for guild: {}", guild_id);
     } else {
-        warn!("Client not set for scheduler");
+        info!("Found {} updates for guild: {}", updated.len(), guild_id);
+        if updated.len() > 5 {
+            send_in_chunks_updates(c, client, &updated).await?;
+        } else {
+            send_in_one_updates(c, client, &updated).await?;
+        }
+
+        for (mod_info, _) in updated {
+            db::subscriptions::update_last_notify(&scheduler.pool, guild_id, mod_info.id)?;
+        }
+    }
+
+    if !failed.is_empty() {
+        c.send_message(&client, |d| {
+            d.content("The following Items could not be updated:".to_string());
+
+            for (_, mod_info) in failed.iter() {
+                d.add_embed(|e| {
+                    e.title(format!("{}, Id: {}", mod_info.name.clone(), mod_info.id));
+                    e.url(format!(
+                        "https://steamcommunity.com/sharedfiles/filedetails/?id={}",
+                        mod_info.id
+                    ));
+                    e
+                });
+            }
+
+            d
+        })
+        .await?;
     }
 
     Ok(())
